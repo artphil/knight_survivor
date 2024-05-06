df99b68b973de11c881ba88c4ab56138e9b5b0e2
artphil <artphil.dev@gmail.com>
2024-05-06 14:00:32 -0300

    feat create the world

--- systems/mob_spawn.gd
+++ systems/mob_spawn.gd
@@ -8,20 +8,31 @@ extends Node2D
 
 var cooldown: float = 0
 
-func _process(delta: float)-> void:
+func _process(delta: float) -> void:
 	cooldown -= delta
 	if cooldown > 0: return
-	
-	var interval =  60.0 / mobs_per_minute
+
+	var interval = 60.0 / mobs_per_minute
 	cooldown = interval
-	
+
+	spown_mob()
+
+func spown_mob():
+	var point = get_point()
+	var world = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = point
+	var result = world.intersect_point(parameters, 1)
+
+	if not result.is_empty(): return
+
 	var index = randi_range(0, creatures.size() - 1)
 	var creature_scene = creatures[index].instantiate()
-	creature_scene.global_position = get_point()
+	creature_scene.global_position = point
 	get_parent().add_child(creature_scene)
-	
 
-func get_point()-> Vector2:
+
+func get_point() -> Vector2:
 	path.progress_ratio = randf()
 	return path.global_position
-	
+
